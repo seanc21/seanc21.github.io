a495eb1bbc25ea97fb0b7d67e783bcb73dd868d9
Sean <scoh21@basecampcodingacademy.org>
2021-02-18 23:45:20 -0600

    changed project section appearance

---
+++ script.js
@@ -0,0 +1,14 @@
+function UnitOne() { 
+    window.open( 
+    "https://github.com/BaseCampCoding/fundamentals-of-programming-pt-1-unit-project-travis-and-sean", "_blank"); 
+}
+
+function UnitTwo() { 
+    window.open( 
+    "https://github.com/BaseCampCoding/unit-project-2-ethan-sean", "_blank"); 
+}
+
+function SideProject() { 
+    window.open( 
+    "https://codepen.io/seanc21/full/PoGPYmO", "_blank"); 
+}
